5cd02de31da7ecb834682c135a553b7f473dfce5
FlowerLSH <68013849+FlowerLSH@users.noreply.github.com>
2020-07-24 22:24:06 +0900

    Update 문자열 마음데로 정렬하기.py

--- 문자열 마음데로 정렬하기.py
+++ 문자열 마음데로 정렬하기.py
@@ -15,3 +15,13 @@ def solution(strings, n):
         answer.extend(emp)
     
         return answer
+    
+    
+def solution(strings, n):
+    answer = []
+    for i in range(len(strings)):
+        strings[i] = strings[i][n] + strings[i]
+    strings.sort()    
+    for j in strings:
+        answer.append(j[1:])
+    return answer
