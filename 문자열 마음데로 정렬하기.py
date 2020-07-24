3b38af6c9f2b911603345e872f60970655958d48
FlowerLSH <68013849+FlowerLSH@users.noreply.github.com>
2020-07-24 22:22:49 +0900

    Create 문자열 마음데로 정렬하기.py

---
+++ 문자열 마음데로 정렬하기.py
@@ -0,0 +1,17 @@
+def solution(strings, n):
+    answer = []
+    index = dict()
+    arr1 = []
+    for i in strings:
+        index[i] = i[n]
+        arr1.append(i[n])
+    arr = list(set(arr1))
+    arr.sort()
+    for j in arr:
+        emp = []
+        for k in index:
+            if index[k] == j: emp.append(k)
+        emp.sort()
+        answer.extend(emp)
+    
+        return answer
